ad66cb95092bf3e40b14c9feb39518061ba806c5
Eduardo Calderon <educr6@gmail.com>
2021-03-26 19:56:33 -0400

    created timestamp project

--- roadmap/Learning a language: Javascript/fcc-apis-microservices/timestamp/server.js
+++ roadmap/Learning a language: Javascript/fcc-apis-microservices/timestamp/server.js
@@ -1,10 +1,21 @@
 let express = require("express");
+let timestamp = require("./timestamp");
 let app = express();
 
 const PORT = 3000;
 
 app.get("/hello-world", (req, res) => {
   res.json({ message: "Hello World" });
+});
+
+app.get("/api/timestamp/:date", (req, res) => {
+  let result = timestamp(req.params.date);
+  res.json(result);
+});
+
+app.get("/api/timestamp/", (req, res) => {
+  let result = timestamp();
+  res.json(result);
 });
 
 let listener = app.listen(PORT || 3000, () => {
---
+++ roadmap/Learning a language: Javascript/fcc-apis-microservices/timestamp/timestamp.js
@@ -0,0 +1,42 @@
+const INVALID_DATE = "Invalid Date";
+
+const isDateInvalid = (date) => {
+  return date.toUTCString() === INVALID_DATE;
+};
+
+const stringHasOnlyNumericCharacters = (str) => {
+  return /^\d+$/.test(str);
+};
+
+const getUnixTimestamp = (dateObj) => {
+  return Math.round(dateObj.getTime() / 1);
+};
+
+const createResponseObj = (dateStr) => {
+  let result = {};
+
+  if (dateStr === undefined) {
+    dateStr = Date.now().toString();
+  }
+
+  if (stringHasOnlyNumericCharacters(dateStr)) {
+    dateStr = parseInt(dateStr);
+  }
+
+  let dateObj = new Date(dateStr);
+
+  if (isDateInvalid(dateObj)) {
+    return { error: INVALID_DATE };
+  }
+
+  let unixTimeStamp = getUnixTimestamp(dateObj);
+
+  result = {
+    utc: dateObj.toUTCString(),
+    unix: unixTimeStamp,
+  };
+
+  return result;
+};
+
+module.exports = createResponseObj;
