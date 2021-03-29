9199adc142d2882f95fdbccc3353582bdef9a471
Eduardo Calderon <educr6@gmail.com>
2021-03-29 15:09:28 -0400

    from the fcc template, added  the whoami method

--- roadmap/Learning a language: Javascript/fcc-apis-microservices/request-header-parser/server.js
+++ roadmap/Learning a language: Javascript/fcc-apis-microservices/request-header-parser/server.js
@@ -1,21 +1,40 @@
+// server.js
+// where your node app starts
+
+// init project
 require("dotenv").config();
-let express = require("express");
-let app = express();
+const requestIp = require("request-ip");
+var express = require("express");
+var app = express();
 
-const PORT = 3000;
-
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
 var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/hello-world", (req, res) => {
-  res.json({ message: "Hello World" });
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-let listener = app.listen(PORT || 3000, () => {
+app.get("/api/whoami", (req, res) => {
+  const clientIp = requestIp.getClientIp(req);
+  res.json({
+    ipaddress: clientIp,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
