9e2b2aaa49b2e14c61a2b21410e9e63ce34524bd
Eduardo Calderon <educr6@gmail.com>
2021-03-29 14:38:12 -0400

    cloned fcc sample repo

--- roadmap/Learning a language: Javascript/fcc-apis-microservices/request-header-parser/server.js
+++ roadmap/Learning a language: Javascript/fcc-apis-microservices/request-header-parser/server.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
 const PORT = 3000;
+
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 app.get("/hello-world", (req, res) => {
   res.json({ message: "Hello World" });
