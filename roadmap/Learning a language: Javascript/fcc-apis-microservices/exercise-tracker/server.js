dda8a2d6de5ac966dceadd6702297dabbe73eda0
Eduardo Calderon <educr6@gmail.com>
2021-03-30 20:37:10 -0400

    /user-new endpoint ready

--- roadmap/Learning a language: Javascript/fcc-apis-microservices/exercise-tracker/server.js
+++ roadmap/Learning a language: Javascript/fcc-apis-microservices/exercise-tracker/server.js
@@ -1,13 +1,51 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const express = require("express");
-const router = express.Router();
+const createUser = require("./user").createUser;
+const getUsers = require("./user").getUsers;
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(
+    req.method + " " + req.path + " - " + "the body was: " + req.body
+  );
+  next();
+});
 
 app.get("/hello-world", (req, res) => {
   res.json({ message: "Hello World Eduardo" });
 });
+
+app.get("/is-mongoose-ok", (req, res) => {
+  if (mongoose) {
+    res.json({ isMongooseOk: !!mongoose.connection.readyState });
+  } else {
+    res.json({ isMongooseOk: false });
+  }
+});
+
+router.post("/new-user", (req, res) => {
+  const username = req.body.username;
+  console.log("This is the username: ", username);
+
+  createUser(username, (err, data) => {
+    if (err) {
+      res.status(400);
+      res.json({ message: "Something happened" });
+      return;
+    }
+
+    console.log(data);
+    res.json(data);
+  });
+});
+
+app.use("/api/exercise", router);
 
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
---
+++ roadmap/Learning a language: Javascript/fcc-apis-microservices/exercise-tracker/user.js
@@ -0,0 +1,38 @@
+let mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  username: { type: String, required: true },
+});
+
+var User = mongoose.model("User", userSchema);
+
+const createUser = (username, done) => {
+  var newUser = new User({ username: username });
+
+  newUser.save((err, data) => {
+    if (err) {
+      return done(err);
+    }
+
+    done(null, data);
+  });
+};
+
+const getUsers = (done) => {
+  Url.find().exec((err, data) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, data);
+  });
+};
+
+exports.UserModel = User;
+exports.createUser = createUser;
+exports.getUsers = getUsers;
